994a2b7bc6517e199ee3803f1104aa34d19dbe76
hailey-royle <hailey.royle@proton.me>
2025-10-01 23:31:48 -0600

    reorg

--- main.c
+++ main.c
@@ -39,14 +39,6 @@ struct liv {
 };
 struct liv liv;
 
-void ValidateArgs(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("Format: $ liv <fileName>");
-        exit(1);
-    }
-    liv.fileName = argv[1];
-}
-
 void SetLineLength() {
     table.lineLength = 0;
     while (*(table.piece[table.linePiece].start + table.lineStart + table.lineLength) != '\n') {
@@ -55,50 +47,6 @@ void SetLineLength() {
     if (table.lineLength > 0) {
         table.lineLength--;
     }
-}
-
-void LoadFile() {
-    size_t size = 0;
-    FILE* filePointer = fopen(liv.fileName, "r");
-    if (filePointer == NULL) {
-        printf("File: '%s' not found", liv.fileName);
-        exit(1);
-    }
-    int fileLength = getdelim(&table.original, &size, '\0', filePointer);
-    fclose(filePointer);
-    table.piece[0].start = table.original;
-    table.piece[0].length = fileLength;
-    table.lineNumber++;
-    SetLineLength();
-}
-
-void DisableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &NormalTermios);
-    printf(END_ALT_SCREEN);
-}
-
-void EnableRawMode() {
-    printf(START_ALT_SCREEN);
-    tcgetattr(STDIN_FILENO, &NormalTermios);
-    struct termios RawTermios = NormalTermios;
-    RawTermios.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    RawTermios.c_oflag &= ~(OPOST);
-    RawTermios.c_cflag |= (CS8);
-    RawTermios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &RawTermios);
-    atexit(DisableRawMode);
-}
-
-void GetScreenSize() {
-    struct winsize ws;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
-    liv.columns = ws.ws_col - COLUMN_OFFSET;
-    liv.rows = ws.ws_row - BOTTOM_OFFSET;
-}
-
-void InitScreen() {
-    EnableRawMode();
-    GetScreenSize();
 }
 
 void GetLine(char* buffer, int remaining, int piece, int offset) {
@@ -188,29 +136,6 @@ void GetLineRelitive(char* buffer, int relitivity) {
     GetLine(&buffer[COLUMN_OFFSET], liv.columns, piece, offset);
 }
 
-void FormatScreenLine(char* buffer, int row) {
-    if (row == liv.rows / 2) {
-        sprintf(buffer, "%-3d ", table.lineNumber);
-    } else {
-        sprintf(buffer, "%3d ", abs(row - (liv.rows / 2)));
-    }
-    GetLineRelitive(buffer, row - (liv.rows / 2));
-}
-
-void RefreshScreen() {
-    printf(CURSOR_HOME);
-    printf(ERASE_SCREEN);
-    for (int row = 1; row <= liv.rows; row++) {
-        char buffer[liv.columns + COLUMN_OFFSET + 4];
-        FormatScreenLine(buffer, row);
-        printf("\x1b[%d;0H%s", row, buffer);
-    }
-    printf("\x1b[%d;0H-%s-", liv.rows + 1, liv.fileName);
-    printf("\x1b[%d;0Hliv :)", liv.rows + 2);
-    printf("\x1b[%d;%dH", liv.rows / 2, table.lineCursor + COLUMN_OFFSET + 1);
-    fflush(stdout);
-}
-
 void LineNext() {
     int start = table.lineStart;
     int piece = table.linePiece;
@@ -277,6 +202,83 @@ void CursorRight() {
     if (table.lineCursor < table.lineLength) { table.lineCursor++; }
 }
 
+void ValidateArgs(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Format: $ liv <fileName>");
+        exit(1);
+    }
+    liv.fileName = argv[1];
+}
+
+void LoadFile() {
+    size_t size = 0;
+    FILE* filePointer = fopen(liv.fileName, "r");
+    if (filePointer == NULL) {
+        printf("File: '%s' not found", liv.fileName);
+        exit(1);
+    }
+    int fileLength = getdelim(&table.original, &size, '\0', filePointer);
+    fclose(filePointer);
+    table.piece[0].start = table.original;
+    table.piece[0].length = fileLength;
+    table.lineNumber++;
+    SetLineLength();
+}
+
+void DisableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &NormalTermios);
+    printf(END_ALT_SCREEN);
+}
+
+void EnableRawMode() {
+    printf(START_ALT_SCREEN);
+    tcgetattr(STDIN_FILENO, &NormalTermios);
+    struct termios RawTermios = NormalTermios;
+    RawTermios.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    RawTermios.c_oflag &= ~(OPOST);
+    RawTermios.c_cflag |= (CS8);
+    RawTermios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &RawTermios);
+    atexit(DisableRawMode);
+}
+
+void GetScreenSize() {
+    struct winsize ws;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+    liv.columns = ws.ws_col - COLUMN_OFFSET;
+    liv.rows = ws.ws_row - BOTTOM_OFFSET;
+}
+
+void InitLiv(int argc, char* argv[]) {
+    ValidateArgs(argc, argv);
+    LoadFile();
+    EnableRawMode();
+    GetScreenSize();
+}
+
+void FormatScreenLine(char* buffer, int row) {
+    if (row == liv.rows / 2) {
+        sprintf(buffer, "%-3d ", table.lineNumber);
+    } else {
+        sprintf(buffer, "%3d ", abs(row - (liv.rows / 2)));
+    }
+    GetLineRelitive(buffer, row - (liv.rows / 2));
+}
+
+void RefreshScreen() {
+    printf(CURSOR_HOME);
+    printf(ERASE_SCREEN);
+    for (int row = 1; row <= liv.rows; row++) {
+        char buffer[liv.columns + COLUMN_OFFSET + 4];
+        FormatScreenLine(buffer, row);
+        printf("\x1b[%d;0H%s", row, buffer);
+    }
+    printf("\x1b[%d;0H-%s-", liv.rows + 1, liv.fileName);
+    printf("\x1b[%d;0Hliv :)", liv.rows + 2);
+    printf("\x1b[%d;%dH", liv.rows / 2, table.lineCursor + COLUMN_OFFSET + 1);
+    fflush(stdout);
+}
+
 void ProssesKeyPress() {
     char key;
     read(STDIN_FILENO, &key, 1);
@@ -295,9 +297,7 @@ void RunLiv() {
 }
 
 int main(int argc, char* argv[]) {
-    ValidateArgs(argc, argv);
-    LoadFile();
-    InitScreen();
+    InitLiv(argc, argv);
     RunLiv();
     return 0;
 }
